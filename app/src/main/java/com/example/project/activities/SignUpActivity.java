06e6ac4aab38511d68a779a36a9e0e6f9a36238b
Utsav Dudhat <und1310@gmail.com>
2023-02-04 21:23:22 +0530

    Login and signup page designed.

---
+++ app/src/main/java/com/example/project/activities/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.example.project.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+}
